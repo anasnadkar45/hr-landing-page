4db73a86c8dfbbaa9b3cb88f42f7c84b775eac45
anasnadkar45 <anasnadkar23@gmail.com>
2024-10-19 08:33:35 +0530

    Project completed

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import { Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import Logo from "../public/Logo.svg";
+import Logo from "../public/Logo.png";
 import Link from "next/link";
 
 export const Navbar = () => {
